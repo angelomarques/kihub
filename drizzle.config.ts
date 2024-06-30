78143166e7d3b4cc25b03ddead9006e475af9dae
Ângelo Marques <angeloemanuelmarques@gmail.com>
2024-06-29 22:23:03 -0300

    setup database with drizzle

---
+++ drizzle.config.ts
@@ -0,0 +1,11 @@
+import { env } from "@/env.mjs";
+import { type Config } from "drizzle-kit";
+
+export default {
+  schema: "./src/server/db/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: env.POSTGRES_URL,
+  },
+  tablesFilter: ["kihub_*"],
+} satisfies Config;
---
+++ src/server/db/schema.ts
@@ -0,0 +1,31 @@
+// Example model schema from the Drizzle docs
+// https://orm.drizzle.team/docs/sql-schema-declaration
+
+import { pgTableCreator } from "drizzle-orm/pg-core";
+
+/**
+ * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
+ * database instance for multiple projects.
+ *
+ * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
+ */
+export const createTable = pgTableCreator((name) => `kihub_${name}`);
+
+// export const images = createTable(
+//   "image",
+//   {
+//     id: serial("id").primaryKey(),
+//     name: varchar("name", { length: 256 }).notNull(),
+//     url: varchar("url", { length: 256 }).notNull(),
+
+//     userId: varchar("userId", { length: 256 }).notNull(),
+
+//     createdAt: timestamp("created_at", { withTimezone: true })
+//       .default(sql`CURRENT_TIMESTAMP`)
+//       .notNull(),
+//     updatedAt: timestamp("updatedAt", { withTimezone: true }),
+//   },
+//   (example) => ({
+//     nameIndex: index("name_idx").on(example.name),
+//   }),
+// );
