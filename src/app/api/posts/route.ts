02e97f750a6bad77f5d90f5919b32536a7ceb5b6
Ângelo Marques <angeloemanuelmarques@gmail.com>
2024-07-06 16:45:11 -0300

    create post form api endpoint

---
+++ src/app/api/posts/route.ts
@@ -0,0 +1,31 @@
+import { PostFormSchema } from "@/lib/schemas/post-form";
+import { db } from "@/server/db";
+import { posts } from "@/server/db/schema";
+import { getAuthenticatedUser } from "@/server/users/queries";
+import { ZodError } from "zod";
+
+export async function POST(request: Request) {
+  try {
+    const user = await getAuthenticatedUser();
+
+    if (!user) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+
+    const body = await request.json();
+    const { content } = PostFormSchema.parse(body);
+
+    const post = await db.insert(posts).values({
+      authorId: user.id,
+      content: content,
+    });
+
+    return Response.json(post);
+  } catch (error) {
+    if (error instanceof ZodError) {
+      return new Response(JSON.stringify(error.flatten()), { status: 422 });
+    }
+
+    return new Response("Internal Server Error", { status: 500 });
+  }
+}
