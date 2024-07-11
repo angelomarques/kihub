8a4b5cdcb1af9eddf428348decd17894875b385a
Ângelo Marques <angeloemanuelmarques@gmail.com>
2024-07-11 19:57:18 -0300

    remove warning logs on postform dialog

--- src/app/_components/post-form-dialog.tsx
+++ src/app/_components/post-form-dialog.tsx
@@ -2,7 +2,12 @@
 
 import { PostForm } from "@/components/post-form";
 import { Button } from "@/components/ui/button";
-import { Dialog, DialogContent } from "@/components/ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogTitle,
+} from "@/components/ui/dialog";
 import { UsersTable } from "@/server/db/types";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 import { usePathname, useRouter } from "next/navigation";
@@ -19,15 +24,17 @@ export function PostFormDialog({ user }: Props) {
   return (
     <Dialog open={isOpen} onOpenChange={() => router.back()}>
       <DialogContent>
-        <Button
-          className="ml-auto flex"
-          variant="ghost"
-          size="icon"
-          onClick={() => router.back()}
-        >
-          <XMarkIcon className="h-5 w-5" />
-          <span className="sr-only">Close</span>
-        </Button>
+        <div className="flex items-center justify-between">
+          <DialogTitle className="text-lg font-medium">New Post</DialogTitle>
+          <Button variant="ghost" size="icon" onClick={() => router.back()}>
+            <XMarkIcon className="h-5 w-5" />
+            <span className="sr-only">Close</span>
+          </Button>
+        </div>
+
+        <DialogDescription className="sr-only">
+          Write a new post
+        </DialogDescription>
 
         <PostForm user={user} />
       </DialogContent>
